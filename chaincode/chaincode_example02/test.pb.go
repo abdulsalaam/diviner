073a2ca309ea1720c2cc9a25505429cc420dada5
kigi <kigi.chang@gmail.com>
2017-11-21 22:39:15 +0800

    change package name

--- chaincode/chaincode_example02/test.pb.go
+++ chaincode/chaincode_example02/test.pb.go
@@ -10,7 +10,7 @@ It is generated from these files:
 It has these top-level messages:
 	Test
 */
-package chaincode_example02
+package main
 
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
